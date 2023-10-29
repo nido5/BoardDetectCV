bfe595d6b54dfb61f39297cd56bd6263fea2bf42
Nick Drobec <nido5.nick@gmail.com>
2023-10-29 15:33:36 +0100

    asd

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ async def capture_and_broadcast():
                         print("Error sending image")
         except ValueError: print("camera reading error", ValueError)
 
-        await asyncio.sleep(0.2)  # Capture an image every 100ms
+        await asyncio.sleep(0.1)  # Capture an image every 100ms
 
 
 async def broadcast(data):
